a83cfbefd095983b8d47f2d48243deaf8d62ffbf
RosarioRu <mariarruval@gmail.com>
2022-08-26 16:17:12 -0700

    Update controllers with blank files for PierreTreats project.

--- PierreTreats/Controllers/HomeController.cs
+++ PierreTreats/Controllers/HomeController.cs
@@ -18,20 +18,10 @@ namespace PierreTreats.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [HttpGet("/")]
+        public ActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
