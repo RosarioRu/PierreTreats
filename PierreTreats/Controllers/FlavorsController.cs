4641c643e00cda648f21e7c941abe7cbd30aca77
RosarioRu <mariarruval@gmail.com>
2022-08-26 20:21:13 -0700

    Add controller action for Index view with list of all instanes of Flavor passed into view.

---
+++ PierreTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq; 
+using PierreTreats.Models;
+using Microsoft.EntityFrameworkCore; 
+
+namespace PierreTreats.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    
+    private readonly PierreTreatsContext _db;
+
+    public FlavorsController(PierreTreatsContext db) 
+    {
+      _db = db;
+    }
+
+    //below we are able to access all Flavor(s) in list form by usign LINQ ToList()
+    //_db's value is db, which is an instance of DbContext class. It holds reference to the database.
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList()); 
+    } 
+
+  }
+}
