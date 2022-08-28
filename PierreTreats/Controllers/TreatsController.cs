7a36380a0de7f87361ba0cba66967088547cc6f3
RosarioRu <mariarruval@gmail.com>
2022-08-27 21:24:20 -0700

    Add DeleteFlavorTreat route that will remove the join entity but NOT the flavor or treat itself.

--- PierreTreats/Controllers/TreatsController.cs
+++ PierreTreats/Controllers/TreatsController.cs
@@ -97,6 +97,15 @@ namespace PierreTreats.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpPost, ActionName("DeleteFlavorTreat")]
+    public ActionResult DetailsAgain(int joinId)
+    {
+      var joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }   
+
     
     
   }
